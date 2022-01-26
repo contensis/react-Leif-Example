02227728823e46ba7fa54380e14bba96fd0d9c30
Jon Maskrey <j.maskrey@zengenti.com>
2022-01-26 12:43:45 +0000

    update error and isLoaded to be inline with the listing example

--- src/components/BlogItem.js
+++ src/components/BlogItem.js
@@ -26,46 +26,50 @@ const BlogItem = () => {
             )
     }, [params])
 
-    if (error) return <div>Error</div>
-    if (!isLoaded) return <div>Loading...</div>
+    if (error) {
+        return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+        return <div>Loading...</div>;
+    } else {
 
-    return (
-        <>
-            <div className="blog-hero">
-                <h1 className="blog-hero__title">{blog.entryTitle}</h1>
-                {blog.thumbnailImage && <img className="blog-hero__img" src={'http://live.leif.zenhub.contensis.cloud' + blog.thumbnailImage.asset.sys.uri} alt={blog.thumbnailImage.altText} />}
-            </div>
-            <div className="profile">
-                {blog.author && <img className="profile__img" src={'http://live.leif.zenhub.contensis.cloud' + blog.author.photo.asset.sys.uri} alt={blog.author.photo.altText} />}
-                {blog.author && <span className="profile__name">{blog.author.entryTitle}</span>}
-                {blog.category && <span className="category">{blog.category.entryTitle}</span>}
-            </div>
-            <div className="blog__content">
-                {blog.leadParagraph && <p className="lead">{blog.leadParagraph}</p>}
+        return (
+            <>
+                <div className="blog-hero">
+                    <h1 className="blog-hero__title">{blog.entryTitle}</h1>
+                    {blog.thumbnailImage && <img className="blog-hero__img" src={'http://live.leif.zenhub.contensis.cloud' + blog.thumbnailImage.asset.sys.uri} alt={blog.thumbnailImage.altText} />}
+                </div>
+                <div className="profile">
+                    {blog.author && <img className="profile__img" src={'http://live.leif.zenhub.contensis.cloud' + blog.author.photo.asset.sys.uri} alt={blog.author.photo.altText} />}
+                    {blog.author && <span className="profile__name">{blog.author.entryTitle}</span>}
+                    {blog.category && <span className="category">{blog.category.entryTitle}</span>}
+                </div>
+                <div className="blog__content">
+                    {blog.leadParagraph && <p className="lead">{blog.leadParagraph}</p>}
 
-                {blog.postBody.map((field, idx) => {
-                    switch (field.type) {
-                        case 'markup':
-                            return (
-                                <div key={idx} dangerouslySetInnerHTML={{ __html: field.value }} ></div>
-                            );
-                        case 'image':
-                            return (
-                                <div key={idx} className="inline-img">
-                                    <img className="inline-img__img" src={'http://live.leif.zenhub.contensis.cloud' + field.value.asset.sys.uri} alt={field.value.altText} />
-                                    <div className="inline-img__content">
-                                        <h2 className="inline-img__title">{field.value.caption}</h2>
+                    {blog.postBody.map((field, idx) => {
+                        switch (field.type) {
+                            case 'markup':
+                                return (
+                                    <div key={idx} dangerouslySetInnerHTML={{ __html: field.value }} ></div>
+                                );
+                            case 'image':
+                                return (
+                                    <div key={idx} className="inline-img">
+                                        <img className="inline-img__img" src={'http://live.leif.zenhub.contensis.cloud' + field.value.asset.sys.uri} alt={field.value.altText} />
+                                        <div className="inline-img__content">
+                                            <h2 className="inline-img__title">{field.value.caption}</h2>
+                                        </div>
                                     </div>
-                                </div>
-                            );
-                        default:
-                            break;
-                    }
-                    return null
-                })}
-            </div>
-        </>
-    )
+                                );
+                            default:
+                                break;
+                        }
+                        return null
+                    })}
+                </div>
+            </>
+        )
+    }
 
 }
 
